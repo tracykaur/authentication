246a941d447e6892c289edfcb804565b1793ee97
Scott Dixon <scott@howrefreshing.com.au>
2017-12-06 14:35:05 +1000

    Secure /products

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const Product = require('../models/Product')
+const { requireJWT } = require('../middleware/auth')
 
 const router = new express.Router()
 
-router.get('/products', (req, res) => {
+router.get('/products', requireJWT, (req, res) => {
   Product.find().then((products) => {
     res.send(products)
   })
