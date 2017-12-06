c1fb9ecca890d89c03d50a11c0a84fb4b976f5de
Scott Dixon <scott@howrefreshing.com.au>
2017-12-06 13:25:08 +1000

    User model

---
+++ api/models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('./init')
+const passportLocalMongoose = require('passport-local-mongoose')
+
+const userSchema = new mongoose.schema({
+  firstName: String,
+  lastName: String
+})
+
+userSchema.plugin(passportLocalMongoose, {
+  usernameField: 'email', // Use email not username
+  usernameLowerCase: true, // Treat emails as case-insensitive
+  session: false // We'll use JWT
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
